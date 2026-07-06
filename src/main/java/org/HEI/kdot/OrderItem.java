3abcd1ee500556283588052ce416a72e3b83bca1
K. <148479852+Kiady06@users.noreply.github.com>
2026-07-06 08:39:53 +0300

    feat : add getTotalItemPrice OrderItem

--- src/main/java/org/HEI/kdot/OrderItem.java
+++ src/main/java/org/HEI/kdot/OrderItem.java
@@ -3,10 +3,16 @@ package org.HEI.kdot;
 import lombok.Builder;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 public class OrderItem {
     private String orderId;
     private int quantity;
     private Product product;
+
+    public BigDecimal getTotalItemPrice() {
+        return product.getCurrentPrice().multiply(new BigDecimal(quantity));
+    }
 }
